f00abb5a13bc0b2e03e2dab8e3b7105b5877f9ec
Harshalik12 <harshalikurapati76@gmail.com>
2025-08-01 19:34:04 +0530

    testing

---
+++ testings.py
@@ -0,0 +1,9 @@
+def even_odd(a):
+      if a%2==0:
+	      print('Even number')
+	  else:
+	      print('Odd')
+def int_filter(*args):
+        for i in args:
+		    if type(i)==int:
+			         print(i)__
